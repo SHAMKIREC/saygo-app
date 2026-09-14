e0af8f93e6bb84176d8190f430bf7e55ac8bc581
SHAMKIREC <konfeta.tvix@gmail.com>
2026-09-14 18:40:23 +0400

    fix: refresh course curriculum cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='saygo-v5';
+const CACHE='saygo-v6';
 const ASSETS=['./','./index.html','./lessons.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
