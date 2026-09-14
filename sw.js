548fba6a0713e0e6e5c5265e4b41557be5899bb5
SHAMKIREC <konfeta.tvix@gmail.com>
2026-09-14 18:41:13 +0400

    fix: refresh SAYGO final exam release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='saygo-v7';
+const CACHE='saygo-v8';
 const ASSETS=['./','./index.html','./lessons.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
