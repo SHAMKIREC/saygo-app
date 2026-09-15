812c99173cf972d7dea118fdb7eaf7899ee4caf8
SHAMKIREC <konfeta.tvix@gmail.com>
2026-09-15 20:39:12 +0400

    Use fresh SAYGO assets after updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='saygo-v10';
+const CACHE='saygo-v11';
 const ASSETS=['./','./index.html','./lessons.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
@@ -7,5 +7,5 @@ self.addEventListener('fetch',event=>{
     event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));
     return;
   }
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)));
 });
